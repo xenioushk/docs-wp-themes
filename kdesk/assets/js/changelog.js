a1a4826f65163f33809855921ae2b556273edc63
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-01-04 10:50:16 +0200

    updated the kdesk theme

--- kdesk/assets/js/changelog.js
+++ kdesk/assets/js/changelog.js
@@ -59,13 +59,14 @@ $(function () {
   }
 
   // Change only the version and date
-  var metaData = ["1.3.8", "20/10/2023"]
+  var metaData = ["1.3.9", "04/01/2024"]
   updateMetaInfo(metaData)
 
   // Just add a new row.
   // @sample: ["2023, July, 13 - v 1.3.6", ["Updated: BWl Knowledge Base Manager Plugin to 1.4.5", "Fixed: Translation issue of 404 page.", "Updated: Theme compatibility with the latest WordPress."]],
   var changeLogData = [
     // Add a new row below this comment.
+    ["2024, January 04 - v 1.3.9", ["Updated: WPBakery Page Builder to 7.3", "Updated: BWl Knowledge Base Manager Plugin to 1.4.9", "Updated: Ultimate Searchable Accordion - WPBakery Page Builder Addon 1.2.5"]],
     ["2023, October 20 - v 1.3.8", ["Fixed: Breadcrumb issue for kb tags.", "Fixed: Ultimate Searchable Accordion installation issue"]],
     ["2023, October 19 - v 1.3.7", ["Updated: WPBakery Page Builder to 7.1", "Updated: BWl Knowledge Base Manager Plugin to 1.4.8", "Updated: Ultimate Searchable Accordion - WPBakery Page Builder Addon 1.2.3", "Updated: kdesk WPBakery Page Builder Addon 1.3.6"]],
     ["2023, July 13 - v 1.3.6", ["Updated: BWl Knowledge Base Manager Plugin to 1.4.5"]],
