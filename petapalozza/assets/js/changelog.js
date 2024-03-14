5a959d8c8e22c08950c0c44d6f86fa3d09c93e7d
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-03-15 01:58:14 +0200

    updated petapalozza theme doc

--- petapalozza/assets/js/changelog.js
+++ petapalozza/assets/js/changelog.js
@@ -59,12 +59,13 @@ $(function () {
   }
 
   // Change only the version and date
-  var metaData = ["1.2.5", "21/12/2023"]
+  var metaData = ["1.2.6", "15/03/2024"]
   updateMetaInfo(metaData)
 
   // Just add a new row.
   var changeLogData = [
     // Add a new row below this comment.
+    ["2024, March 15 - v 1.2.6", ["Updated: WPBakery Page Builder to 7.5"]],
     ["2023, December 21 - v 1.2.5", ["Updated: WPBakery Page Builder to 7.3", "Updated: Petapalozza Theme WPBakery Page Builder Addon to 1.1.7"]],
     ["2023, November 02 - v 1.2.4", ["Updated: WPBakery Page Builder to 7.1"]],
   ]
