5b696c92d9eaa1ff00556b0eb4f881c44f1bf64a
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-25 11:23:59 +0300

    🔥 Updated: Kdesk 1.4.9 doc.

--- kdesk/assets/js/changelog.js
+++ kdesk/assets/js/changelog.js
@@ -60,14 +60,14 @@ $(function () {
   }
 
   // Change only the version and date
-  var metaData = ["1.4.8", "18/04/2025"]
+  var metaData = ["1.4.9", "25/04/2025"]
   updateMetaInfo(metaData)
 
   // Just add a new row.
   // @sample: ["2023, July, 13 - v 1.3.6", ["Updated: BWl Knowledge Base Manager Plugin to 1.4.5", "Fixed: Translation issue of 404 page.", "Updated: Theme compatibility with the latest WordPress."]],
   var changeLogData = [
     // Add a new row below this comment.
-
+    ["2025, April 24 - v 1.4.9", ["Updated: WPBakery Page Builder to 8.4.1", "Fixed: Translation notice for WP 6.8", "Updated: kdesk WPBakery Page Builder Addon 1.4.1"]],
     ["2025, April 18 - v 1.4.8", ["Updated: WPBakery Page Builder to 8.4", "Updated: BWl Knowledge Base Manager Plugin to 1.5.8", "Added: Templify KB Addon for better template layout management."]],
     ["2025, March 30 - v 1.4.7", ["Updated: WPBakery Page Builder to 8.3.1", "Updated: BWl Knowledge Base Manager Plugin to 1.5.6", "Updated: Ultimate Searchable Accordion - WPBakery Page Builder Addon 1.3.5"]],
     ["2024, December 13 - v 1.4.6", ["Fixed: Search banner block issue for mobile menu.", "Updated: WPBakery Page Builder to 8.1", "Updated: kdesk WPBakery Page Builder Addon 1.4.0"]],
