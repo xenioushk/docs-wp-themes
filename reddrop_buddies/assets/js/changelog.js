513817c867a2af43441cd3c91dac940cb5bb88b0
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2026-02-27 22:57:46 +0200

    update: reddrop buddies wordpress theme doc.

--- reddrop_buddies/assets/js/changelog.js
+++ reddrop_buddies/assets/js/changelog.js
@@ -60,12 +60,13 @@ $(function () {
   }
 
   // Change only the version and date
-  var metaData = ["1.4.2", "08/08/2025"]
+  var metaData = ["1.4.3", "27/02/2026"]
   updateMetaInfo(metaData)
 
   // Just add a new row.
   var changeLogData = [
     // Add a new row below this comment.
+    ["2026, February 27 - v 1.4.3", ["Updated: Reddrop Buddies WPBakery Page Builder Addons to 1.2.7"]],
     ["2025, August 08 - v 1.4.2", ["Updated: WPBakery Page Builder to 8.6.1"]],
     ["2025, June 25 - v 1.4.1", ["Updated: WPBakery Page Builder to 8.5", "Updated: Reddrop Buddies WPBakery Page Builder Addons to 1.2.6"]],
     ["2025, April 25 - v 1.4.0", ["Updated: WPBakery Page Builder to 8.4.1", "Fixed: Translation notice for WP 6.8"]],
